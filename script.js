c298ae09ff36ab31930bf8952161202deb79db2f
Abizer <72496586+abizer2000@users.noreply.github.com>
2024-08-14 12:10:32 +0530

    templatize convert function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const MILESTOKILOMETERS = 1.6093
+const GALLONSTOLITERS = 3.7854
+const POUNDSTOKILOGRAMS = 0.4535
+const OUNCESTOGRAMS = 28.3495
+const USDTOINR = 84 // Approximate value
+
 function fahrenheitToCelsius(fahrenheit) {
     return ((fahrenheit - 32) * 5 / 9).toFixed(1);
 }
@@ -6,44 +12,8 @@ function celsiusToFahrenheit(celsius) {
     return ((celsius * 9 / 5) + 32).toFixed(1);
 }
 
-function milesToKilometers(miles) {
-    return (miles * 1.6093).toFixed(2);
-}
-
-function kilometersToMiles(kilometers) {
-    return (kilometers / 1.6093).toFixed(2);
-}
-
-function gallonsToLiters(gallons) {
-    return (gallons * 3.7854).toFixed(2);
-}
-
-function litersToGallons(liters) {
-    return (liters / 3.7854).toFixed(2);
-}
-
-function poundsToKilograms(pounds) {
-    return (pounds * 0.4535).toFixed(2);
-}
-
-function kilogramsToPounds(kilograms) {
-    return (kilograms / 0.4535).toFixed(2);
-}
-
-function ouncesToGrams(ounces) {
-    return (ounces * 28.3495).toFixed(2);
-}
-
-function gramsToOunces(grams) {
-    return (grams / 28.3495).toFixed(2);
-}
-
-function USDToINR(usd) {
-    return (usd * 84).toFixed(2);
-}
-
-function INRToUSD(inr) {
-    return (inr / 84).toFixed(2);
+function convert(value, conversionFactor) {
+    return (value * conversionFactor).toFixed(2)
 }
 
 function handleConversion(event) {
@@ -65,34 +35,34 @@ function handleConversion(event) {
             otherInput.value = celsiusToFahrenheit(value);
             break;
         case 'miles':
-            otherInput.value = milesToKilometers(value);
+            otherInput.value = convert(value, MILESTOKILOMETERS);
             break;
         case 'kilometers':
-            otherInput.value = kilometersToMiles(value);
+            otherInput.value = convert(value, 1 / MILESTOKILOMETERS);
             break;
         case 'gallons':
-            otherInput.value = gallonsToLiters(value);
+            otherInput.value = convert(value, GALLONSTOLITERS);
             break;
         case 'liters':
-            otherInput.value = litersToGallons(value);
+            otherInput.value = convert(value, 1 / GALLONSTOLITERS);
             break;
         case 'pounds':
-            otherInput.value = poundsToKilograms(value);
+            otherInput.value = convert(value, POUNDSTOKILOGRAMS);
             break;
         case 'kilograms':
-            otherInput.value = kilogramsToPounds(value);
+            otherInput.value = convert(value, 1 / POUNDSTOKILOGRAMS);
             break;
         case 'ounces':
-            otherInput.value = ouncesToGrams(value);
+            otherInput.value = convert(value, OUNCESTOGRAMS);
             break;
         case 'grams':
-            otherInput.value = gramsToOunces(value);
+            otherInput.value = convert(value, 1 / OUNCESTOGRAMS);
             break;
         case 'usd':
-            otherInput.value = USDToINR(value);
+            otherInput.value = convert(value, USDTOINR);
             break;
         case 'inr':
-            otherInput.value = INRToUSD(value);
+            otherInput.value = convert(value, 1 / USDTOINR);
             break;
     }
 }
