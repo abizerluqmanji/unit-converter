7b6cf1c51c8a8ca94397d45e9808543848cc87a3
Abizer Luqmanji <72496586+abizerluqmanji@users.noreply.github.com>
2025-02-17 19:51:55 -0500

    Update USD/INR rate

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const FLUIDOUNCESTOMILLILITERS = 29.5735;
 const MPGTOKMPL = 0.4251;
 const POUNDSTOKILOGRAMS = 0.4535;
 const OUNCESTOGRAMS = 28.3495;
-const USDTOINR = 88; // Approximate value
+const USDTOINR = 87; // Approximate value
 
 function fahrenheitToCelsius(fahrenheit) {
     return ((fahrenheit - 32) * 5 / 9).toFixed(1);
